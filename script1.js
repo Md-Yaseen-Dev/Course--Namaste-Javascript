2e769ce27ef9010ad96b6bca4bdcce11a27b88c6
mohammedyaseen2211 <ysn6040@gmail.com>
2023-09-05 23:30:30 +0530

    closure

--- script1.js
+++ script1.js
@@ -1,21 +1,41 @@
-// how functions work in js
-var x = 1;
-a();
-b();
-console.log(x);
+// // how functions work in js
+// var x = 1;
+// a();
+// b();
+// console.log(x);
 
 
-function a(){
-    var x =10;
-    console.log(x);
+// function a(){
+//     var x =10;
+//     console.log(x);
+// }
+
+// function b(){
+//     var x =100;
+//     console.log(x)
+// }
+
+
+// // ---shortest js program (empty file)------
+
+
+// closure is a function in a lexical behaviour it remembers even the function is outside the function (or) the function is closed
+
+
+function outer(){
+
+    var a = 10;
+     function inner(){
+     console.log(a)
+    }
+    a=100;
+    return inner;
 }
 
-function b(){
-    var x =100;
-    console.log(x)
-}
+const close = outer();
+console.log(close); // see here the function is closed.
 
+// by calling close variable we can access inner function. as shown below:
 
-// ---shortest js program (empty file)------
-
+close(); // it returns the refernce of a i.e 100. 
 
