499e4d4e77db15b723724d67c743c90fd4b90b44
mohammedyaseen2211 <ysn6040@gmail.com>
2023-09-06 00:50:32 +0530

    setTimeout

--- script1.js
+++ script1.js
@@ -1,41 +1,87 @@
-// // how functions work in js
-// var x = 1;
-// a();
-// b();
-// console.log(x);
+// // // how functions work in js
+// // var x = 1;
+// // a();
+// // b();
+// // console.log(x);
 
 
-// function a(){
-//     var x =10;
-//     console.log(x);
+// // function a(){
+// //     var x =10;
+// //     console.log(x);
+// // }
+
+// // function b(){
+// //     var x =100;
+// //     console.log(x)
+// // }
+
+
+// // // ---shortest js program (empty file)------
+
+// // ---------------------------------------------------------------------------------
+
+// // closure is a function in a lexical behaviour it remembers even the function is outside the function (or) the function is closed
+
+
+// function outer(){
+
+//     var a = 10;
+//      function inner(){
+//      console.log(a)
+//     }
+//     a=100;
+//     return inner;
 // }
 
-// function b(){
-//     var x =100;
-//     console.log(x)
-// }
+// const close = outer();
+// console.log(close); // see here the function is closed.
+
+// // by calling close variable we can access inner function. as shown below:
+
+// close(); // it returns the refernce of a i.e 100. 
 
 
-// // ---shortest js program (empty file)------
+/*--- uses of closures:-
+
+- modules design pattern
+- currying
+- functions like once
+- memorize
+- maintaining state in async world
+- setTimeouts
+- Iterators
+- and many more...
+
+*/
+
+// setTimeOut
 
 
-// closure is a function in a lexical behaviour it remembers even the function is outside the function (or) the function is closed
 
+function y(){
 
-function outer(){
-
-    var a = 10;
-     function inner(){
-     console.log(a)
-    }
-    a=100;
-    return inner;
+    for(let i=1;i <5;i++){
+     
+    setTimeout(function(){
+        console.log(i)
+    }, i * 100)
+}
 }
 
-const close = outer();
-console.log(close); // see here the function is closed.
+y(); // after 100 millsec it is printing i =5 because of closures the function of lexical behaviour i refernce will be 5 so here it is printing 5 for every loop. The loop is working through values but it returns their refernces of i and they dont change when it iterates.
 
-// by calling close variable we can access inner function. as shown below:
+// Note when u use let variable it will run 1,2,3,4,5. because let is a block scope where it iterates the loop with a new reference.
 
-close(); // it returns the refernce of a i.e 100. 
+// function x(){
 
+//     for(var i=1;i <=10;i++){
+//         function closure(a){
+//     setTimeout(function(){
+//         console.log(a)
+//     }, a * 1000)
+// }
+// closure(i)
+// }
+// }
+
+// x(); // after 1 sec it will print.
